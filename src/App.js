9c0fe0c33515e3a76cc0a3533f8f33818f52ae13
DylanVallejo <dylan1811vallejo@gmail.com>
2023-01-08 20:35:42 -0500

    Folder initial structure adn components, adding dependencies react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
 import './App.css';
 
+import {Route,Routes,BrowserRouter} from 'react-router-dom';
+import Index from './pages/Index';
+import Create from './pages/Create';
+import View from './pages/View';
+
+
+
+
 function App() {
   return (
     <div className="App">
-        Kruger Library
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Index/>} />
+          <Route path='/create' element={<Create/>} />
+          <Route path='/view/:bookId' element={<View/>} />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
